b245697b237eb98bfc8fcf1dea48ec2987f670b7
Jax <bbbbbb5544@gmail.com>
2022-04-06 22:12:05 +0800

    0404

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <a href="#" class="head-icon" @click.prevent="openCart">
+      購物車開
+    </a>
+    <a href="#" class="head-icon" @click.prevent="closeCart">
+      購物車關
+    </a>
+    <TestPage></TestPage>
+  </div>
+</template>
+
+<script>
+import TestPage from './pages/TestPage.vue'
+import bus from "./bus.js";
+
+export default {
+  name: 'App',
+  components: {
+    TestPage
+  },
+  methods:{
+    openCart(){
+      bus.$emit("isShow", true);
+    },
+    closeCart(){
+      bus.$emit("isShow", false);
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/pages/TestPage.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    {{isShow}}
+  </div>
+</template>
+
+<script>
+import bus from "../bus.js";
+
+export default {
+  name: 'TestPage',
+  data() {
+    return {
+      isShow: false
+    }
+  },
+  mounted(){
+    bus.$on("isShow", (val) => {
+      this.isShow = val;
+    })
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
